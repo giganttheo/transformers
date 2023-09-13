e9b2d58112ed21bbbfea578567c2a5bc430d723e
gigant <giganttheo@gmail.com>
2023-09-13 14:10:18 +0200

    fixing generation - wip

--- src/transformers/generation/flax_utils.py
+++ src/transformers/generation/flax_utils.py
@@ -610,6 +610,7 @@ class FlaxGenerationMixin:
             """state update fn."""
             model_outputs = model(state.running_token, params=params, **state.model_kwargs)
             logits = model_outputs.logits[:, -1]
+            print(logits)
 
             # apply min_length, ...
             logits = logits_processor(state.sequences, logits, state.cur_len)
