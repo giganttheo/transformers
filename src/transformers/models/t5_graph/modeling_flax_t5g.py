test | fixing the causal attention shift for decoding

src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -478,7 +478,7 @@ class FlaxT5Attention(nn.Module):
 
             if self.causal:
                 # fast decoding for generate requires special attention_mask
-                if self.has_variable("cache", "cached_key"):
+                if False:#self.has_variable("cache", "cached_key"):
                     max_decoder_length = self.variables["cache"]["cached_key"].shape[1]
                     causal_mask = (receivers <= senders) * ~(receivers >= causal_attention_mask_shift) * ~(senders < max_decoder_length)
                 else: