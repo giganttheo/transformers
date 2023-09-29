30e93a65aec523f653b0c807fd6d3382b546b51e
gigant <giganttheo@gmail.com>
2023-09-29 11:44:36 +0200

    test | fixing the causal attention shift for decoding

--- src/transformers/models/t5_graph/modeling_flax_t5g.py
+++ src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -482,7 +482,7 @@ class FlaxT5Attention(nn.Module):
                 # fast decoding for generate requires special attention_mask
                 if self.has_variable("cache", "cached_key"):
                     max_decoder_length = self.variables["cache"]["cached_key"].shape[1]
-                    causal_mask = (receivers <= senders) * (not receivers < causal_attention_mask_shift) * (not senders > max_decoder_length)
+                    causal_mask = (receivers <= senders) * ~(receivers < causal_attention_mask_shift) * ~(senders > max_decoder_length)
                 else:
                     causal_mask = receivers <= senders
                 graph_mask = graph_mask * causal_mask
@@ -526,7 +526,7 @@ class FlaxT5Attention(nn.Module):
                 # fast decoding for generate requires special attention_mask
                 if self.has_variable("cache", "cached_key"):
                     max_decoder_length = self.variables["cache"]["cached_key"].shape[1]
-                    causal_mask = (receivers <= senders) * (not receivers < causal_attention_mask_shift) * (not senders > max_decoder_length)
+                    causal_mask = (receivers <= senders) * ~(receivers < causal_attention_mask_shift) * ~(senders > max_decoder_length)
                 else:
                     causal_mask = receivers <= senders
                 graph_mask = graph_mask * causal_mask
