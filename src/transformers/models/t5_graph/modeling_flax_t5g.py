475589c604ebe4f960343f504a2af341fbeda41a
gigant <giganttheo@gmail.com>
2023-10-02 18:36:36 +0200

    test | fix the use of attention

--- src/transformers/models/t5_graph/modeling_flax_t5g.py
+++ src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -501,12 +501,13 @@ class FlaxT5Attention(nn.Module):
             # During fast autoregressive decoding, we feed one position at a time,
             # and cache the keys and values step by step.
             if self.causal and (self.has_variable("cache", "cached_key") or init_cache):
-                key_states, value_states, attention_mask = self._concatenate_to_cache(
+                key_states, value_states, pad_mask = self._concatenate_to_cache(
                     key_states, value_states, query_states, attention_mask,
                 )
 
             #merge attention mask with graph mask
             attn_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, ids: mask[ids], in_axes=(None, 0)))
+            graph_mask = graph_mask * attn_mask_2_graph_mask(pad_mask, receivers)
             graph_mask = graph_mask * attn_mask_2_graph_mask(attention_mask, receivers) * attn_mask_2_graph_mask(attention_mask, senders)
 
             # replace masked positions with -10_000
