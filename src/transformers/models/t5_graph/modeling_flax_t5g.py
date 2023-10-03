6b7ed146535c0501577df3b632a17ca041356f81
gigant <giganttheo@gmail.com>
2023-10-03 14:10:53 +0200

    test | fix the use of attention

--- src/transformers/models/t5_graph/modeling_flax_t5g.py
+++ src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -407,7 +407,7 @@ class FlaxT5Attention(nn.Module):
             #     (0, 0, causal_attention_mask_shift, 0),
             #     (1, self.n_heads, seq_length, max_decoder_length),
             # )
-            position_bias = position_bias * (receivers >= causal_attention_mask_shift)  #TODO TODO TODO
+            # position_bias = position_bias * (receivers >= causal_attention_mask_shift)  #TODO TODO TODO
         return position_bias
 
     def _create_position_bias(
