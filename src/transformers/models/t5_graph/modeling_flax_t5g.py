e73058998366efdc50644af889a49f56ee35d65b
gigant <giganttheo@gmail.com>
2023-10-04 14:46:53 +0200

    test | fix the use of attention

--- src/transformers/models/t5_graph/modeling_flax_t5g.py
+++ src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -521,7 +521,7 @@ class FlaxT5Attention(nn.Module):
             if pad_mask is not None:
                 #causal cache mask to only attend to the tokens up to the current token
                 pad_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, ids: mask[ids], in_axes=(None, 0)), in_axes=(None, 0))
-                graph_mask = graph_mask * pad_mask_2_graph_mask(pad_mask, receivers) * pad_mask_2_graph_mask(pad_mask, senders)
+                graph_mask = graph_mask * pad_mask_2_graph_mask(pad_mask, senders)
 
         attn_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, ids: mask[ids], in_axes=(None, 0)))
         #merge attention mask with graph mask
