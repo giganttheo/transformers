f7006c569682b5c69459688bec479f1a4ee8f968
gigant <giganttheo@gmail.com>
2023-10-05 14:53:19 +0200

    test | fix the use of attention

--- src/transformers/models/t5_graph/modeling_flax_t5g.py
+++ src/transformers/models/t5_graph/modeling_flax_t5g.py
@@ -591,7 +591,7 @@ class FlaxT5Attention(nn.Module):
 
             if graph_mask is not None:
                 # position_bias = position_bias # + graph_mask
-                causal_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, r, s: mask[r, s]))#, in_axes=(None,0, 0)))
+                causal_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, r, s: mask[r, s], in_axes=(None,0, 0)))
                 position_bias = causal_mask_2_graph_mask(position_bias, receivers, senders)
             
         # if self.has_variable("cache", "cached_key"):
